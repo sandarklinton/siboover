2d938d5c6a4f2075dc878f3a08d2e630d71ba610
zbareno <z.bareno@gmail.com>
2016-04-08 19:30:56 +0700

    View

---
+++ laravel-siboover/resources/views/home-user.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="materialize/css/materialize.css" rel="stylesheet" type="text/css">
+        <!--<link href="materialize/css/login.css" rel="stylesheet" type="text/css">-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <style>
+
+        </style>
+    </head>
+
+    <body>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+        <div class="container">
+            <div class="row">
+                <!-- Dropdown Structure -->
+                <ul id="dropdown1" class="dropdown-content">
+                    <li><a href="#!">Logout</a></li>
+                </ul>
+                <nav>
+                    <div class="nav-wrapper">
+                        <a href="#!" class="brand-logo">Logo</a>
+                        <ul class="right hide-on-med-and-down">
+                            <!-- Dropdown Trigger -->
+                            <li><a class="dropdown-button" href="#!" data-activates="dropdown1">John Doe<i class="material-icons right">arrow_drop_down</i></a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </body>
+</html>
